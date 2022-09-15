a7bd857ba3e54daa631f80ddf2b564eedd88cd54
orbis <infectores@gmail.com>
2022-09-15 14:40:27 +0300

    renaming

--- src/watcher.ts
+++ src/watcher.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import chokidar from "chokidar";
 import { onAdd } from './handler';
 
-const watching: string = process.env.QS_TEMP_CONTENT_DIRS || '';
+const watching: string = process.env.QS_WTC_DIRS || '';
 
 const config = {
   ignored: /(~\$|_).*\.xlsx|.*\.env|.*\.qvf|.*\.swp|.DS_Store/, // ignore files, (witch start on ~$ or _ and end on .xlsx) or .env etc.
